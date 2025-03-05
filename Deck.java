5d8de3e398bcb6a780761c68ac95dfb1142bde69
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-05 22:35:23 +0530

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,38 @@
+// Deck.java
+
+import java.util.*;
+class Deck{
+	ArrayList<Card> deck;
+
+	public Deck(){
+		this.deck = new ArrayList<>();
+	}
+
+	public void createDeck(){
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+		String[] suits = {"Clubs","Diamonds","Spades","Hearts"};
+
+		for (String rank : ranks){
+			for (String suit : suits){
+				deck.add(new Card(rank, suit));
+			}
+		}
+	}
+
+	public void displayDeck(){
+		System.out.println("_____Deck_____");
+		for (Card card : deck){
+			System.out.println(card);
+		}
+	}
+
+	public void shuffleDeck(){
+		System.out.println("Shufflig Deck");
+		Collections.shuffle(deck);
+	}
+
+	public void pickTwo(){
+		System.out.println("");
+	}
+
+}
