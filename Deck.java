fd73b7e5316a135ceaea91cc366073b04c36118d
Shriyans Sahoo <shriyanssahoo2005@gmail.com>
2025-03-05 22:40:10 +0530

    Constructor to create deck in Deck.java

--- Deck.java
+++ Deck.java
@@ -4,9 +4,11 @@ import java.util.*;
 class Deck{
 	ArrayList<Card> deck;
 
-	public Deck(){
-		this.deck = new ArrayList<>();
-	}
+	// Constructor to create deck
+    	public Deck() {
+        	this.deck = new ArrayList<>();
+        	createDeck();
+    	}
 
 	public void createDeck(){
 		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
